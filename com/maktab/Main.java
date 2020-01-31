d10079d2fbefd7873a1f21abc156e677be30df49
M.Madanipour <madanipoor.maryam@gmail.com>
2020-01-31 16:16:55 +0330

    make main method empty

--- com/maktab/Main.java
+++ com/maktab/Main.java
@@ -1,16 +1,12 @@
 package com.maktab;
 
-import com.maktab.entitiy.Person;
-import com.maktab.entitiy.Student;
-
 /*
  * @author M.Madanipour
  */
 public class Main {
 
-    
+
     public static void main(String[] args) {
-        Person person = new Student();
-        person.introduceYourself();
+
     }
 }
---
+++ com/maktab/entitiy/Teacher.java
@@ -0,0 +1,8 @@
+package com.maktab.entitiy;
+
+public class Teacher extends Person{
+    @Override
+    public void introduceYourself() {
+        System.out.println("I am a teacher");
+    }
+}
