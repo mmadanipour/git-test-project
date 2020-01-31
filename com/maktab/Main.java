70579805bc05306a6d29e80e5dfc406a19bf78bf
mmadanipour <44620470+mmadanipour@users.noreply.github.com>
2020-01-31 15:48:39 +0330

    test commit

--- com/maktab/Main.java
+++ com/maktab/Main.java
@@ -5,6 +5,7 @@ import com.maktab.entitiy.Student;
 
 public class Main {
 
+    
     public static void main(String[] args) {
         Person person = new Student();
         person.introduceYourself();
