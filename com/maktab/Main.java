318e421ff4da393084dbf15800d248d3b17e663b
M.Madanipour <madanipoor.maryam@gmail.com>
2020-01-31 16:19:18 +0330

    modify main method

--- com/maktab/Main.java
+++ com/maktab/Main.java
@@ -1,5 +1,12 @@
 package com.maktab;
 
+import com.maktab.entitiy.Person;
+import com.maktab.entitiy.Student;
+import com.maktab.entitiy.Teacher;
+
+import java.util.Arrays;
+import java.util.List;
+
 /*
  * @author M.Madanipour
  */
@@ -7,6 +14,15 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+        Student student = new Student();
+        Teacher teacher = new Teacher();
+        List<Person> personList = Arrays.asList(teacher, student);
+
+        for (Person p:
+                personList) {
+            p.introduceYourself();
+        }
 
     }
 }
